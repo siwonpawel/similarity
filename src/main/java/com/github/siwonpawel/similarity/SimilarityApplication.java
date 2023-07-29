e3225313b6c23e39119b24009184b5db994aca95
Paweł Siwoń <siwonpawel@gmail.com>
2023-07-29 12:14:32 +0200

    project init

---
+++ src/main/java/com/github/siwonpawel/similarity/SimilarityApplication.java
@@ -0,0 +1,13 @@
+package com.github.siwonpawel.similarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/siwonpawel/similarity/SimilarityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.siwonpawel.similarity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
