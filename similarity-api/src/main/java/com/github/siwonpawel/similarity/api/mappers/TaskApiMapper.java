ccc757a1c0416c0fdfe7b48d938d78a93095651a
Paweł Siwoń <siwonpawel@gmail.com>
2023-07-30 21:47:18 +0200

    corrections to api model

--- similarity-api/src/main/java/com/github/siwonpawel/similarity/api/mappers/TaskApiMapper.java
+++ similarity-api/src/main/java/com/github/siwonpawel/similarity/api/mappers/TaskApiMapper.java
@@ -2,11 +2,18 @@ package com.github.siwonpawel.similarity.api.mappers;
 
 import com.github.siwonpawel.similarity.model.TaskDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TaskApiMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "modifiedAt", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "progress", ignore = true)
+    @Mapping(target = "result", ignore = true)
     TaskDto mapToModel(com.github.siwonpawel.similarity.api.v1.model.NewTask source);
 
     TaskDto mapToModel(com.github.siwonpawel.similarity.api.v1.model.Task source);
